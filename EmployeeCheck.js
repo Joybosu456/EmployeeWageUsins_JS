0e176f4eb7e84faf7dde38c73b0dc9a0ebe590fb
JOY <joybosu@gmail.com>
2022-04-08 11:50:51 +0530

    [Joy] adding Daily employee Wage Program file

--- EmployeeCheck.js
+++ EmployeeCheck.js
@@ -1,4 +1,4 @@
-console.log("Welcome to Employee Wage Program using java script")
+/*console.log("Welcome to Employee Wage Program using java script")
 const IS_ABSENT=0;
 let empcheck = Math.floor(Math.random()*10)%2;
 if(empcheck==IS_ABSENT)
@@ -8,4 +8,33 @@ if(empcheck==IS_ABSENT)
 else
 {
 console.log("Employee is Present");
+}*/
+
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+{
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    switch (empCheck) 
+    {
+        case IS_ABSENT:
+            empHrs = 0;
+            break;            
+        case IS_PART_TIME:
+            empHrs = PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            empHrs = FULL_TIME_HOURS; 
+            break;
+        default:
+            empHrs = 0;
+            break;
+    }
+    let empWage = empHrs * WAGE_PER_HOUR; 
+    console.log("Emp Wage: " + empWage);
 }
