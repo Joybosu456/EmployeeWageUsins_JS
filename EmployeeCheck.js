7987088904582a7872f4395f521001255d3c6e81
JOY <joybosu@gmail.com>
2022-04-08 11:59:27 +0530

    [Joy] Adding emp wage till the condition

--- EmployeeCheck.js
+++ EmployeeCheck.js
@@ -71,7 +71,7 @@ const WAGE_PER_HOUR = 20;
 let empWage = empHrs * WAGE_PER_HOUR; 
 console.log("Emp Work Hrs: "+ empHrs +", Emp Wage: " + empWage); 
 }*/
-const IS_ABSENT = 0;
+/*const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -107,4 +107,44 @@ const NO_OF_WORKING_DAYS = 20;
     }
 let empWage = totalEmpHrs * WAGE_PER_HOUR; 
 console.log("Emp Work Hrs: "+ totalEmpHrs +", Emp Wage: " + empWage); 
+}*/
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NO_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 160;
+{
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    while( totalWorkingDays<NO_OF_WORKING_DAYS && totalEmpHrs<=MAX_HOURS_IN_MONTH ) 
+    {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        let empHrs=getWorkHours(empCheck);
+        totalEmpHrs += empHrs;
+        totalWorkingDays++
+    } 
+// Check Employee is present or Absent or part time       
+    function getWorkHours(empCheck)
+    {
+        switch (empCheck) 
+        {
+            case IS_ABSENT:
+                return 0;
+                break;
+            case IS_PART_TIME:
+                return PART_TIME_HOURS; 
+                break;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS; 
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+let empWage = totalEmpHrs * WAGE_PER_HOUR; 
+console.log("Total Days:" + totalWorkingDays + " Total Hrs:" + totalEmpHrs + " Emp Wage:" + empWage); 
 }
